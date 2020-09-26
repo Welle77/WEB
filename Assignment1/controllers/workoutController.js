2d092a8c93059c2eed40921c85367aa8774bd760
Laurits Dyrberg Hansen <201706994@post.au.dk>
2020-09-26 11:51:34 +0200

    changed some things

---
+++ Assignment1/controllers/workoutController.js
@@ -0,0 +1,14 @@
+let {workout} = require('../models/exerciseProgram');
+
+
+module.exports.index = function(req, res){
+    res.render('index',{title:'Exercise Web App'})
+}
+
+module.exports.workout = function(req, res){
+    res.render('workouts',{title:'Workouts'})
+}
+
+module.exports.getWorkouts = function(req, res){
+    
+}
